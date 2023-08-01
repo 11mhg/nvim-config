827a6a425c7bcdade00615083b69260fad538bb6
Hamada Gasmallah <hamada@distributive.network>
2023-08-01 11:19:28 -0400

    fix(lsp_config): Update lsp_config to fix rust_analyzer issue

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -8,6 +8,7 @@ require("mason-lspconfig").setup({
     "vtsls",
     "pyright",
     "yamlls",
+    "rust_analyzer"
   }
 })
 
@@ -46,6 +47,11 @@ require("lspconfig").pyright.setup {
 }
 
 require("lspconfig").yamlls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+require("lspconfig").rust_analyzer.setup {
   on_attach = on_attach,
   capabilities = capabilities,
 }
